8225ae0159887108b0b0373b06c9bbc8f4eb43cc
Jonas Sontheim <js@qlax.de>
2021-11-28 23:21:30 +0100

    upgrade next 12, trying out framer handshake

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  images: {
-    domains: ["images.unsplash.com"],
+  experimental: {
+    urlImports: [
+      "https://framer.com/m/",
+      "https://framerusercontent.com/",
+      "https://ga.jspm.io/",
+      "https://jspm.dev/",
+    ],
   },
 };
--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,61 @@
 import Image from "next/image";
-
+import PicturePreview from "https://framer.com/m/PicturePreview-RRrz.js@LjXvDAgjRtXggpcqlbnP";
 export default function Home() {
   return (
-    <div className="w-full h-screen flex p-8 md:p-12 ">
-      <div className="md:w-4/6 5s:w-full flex flex-col justify-between mr-8 ">
-        <div className="flex flex-row h-full w-full ">
-          <div className=" h-full w-full flex flex-col font-lato font-light text-2xl -space-y-1 uppercase text-black">
-            <p className="tracking-tight">Jonas</p>
-            <p className="tracking-tighter">Sontheim</p>
-            <div className=" w-screen pr-8 h-full md:hidden ">
-              <div className="mt-5 h-full w-full">
-                <Image width={720} height={1024} src="/kickflipMork.jpg" />
+    <div>
+      <div className="flex w-full h-screen p-8 md:p-12 ">
+        <div className="flex flex-col justify-between mr-8 md:w-4/6 5s:w-full ">
+          <div className="flex flex-row w-full h-full ">
+            <div className="flex flex-col w-full h-full -space-y-1 text-2xl font-light text-black uppercase font-lato">
+              <p className="tracking-tight">Jonas</p>
+              <p className="tracking-tighter">Sontheim</p>
+              <div className="w-screen h-full pr-8 md:hidden">
+                <div className="w-full h-full my-4 ">
+                  <Image width={720} height={1024} src="/kickflipMork.jpg" />
+                </div>
               </div>
+            </div>
+
+            <div className="hidden w-1/2 h-full text-4xl text-right uppercase font-cormorant md:text-8xl md:block ">
+              hof
             </div>
           </div>
 
-          <div className="w-1/2 h-full text-right font-cormorant text-4xl md:text-8xl uppercase hidden md:block ">
-            hof
+          <div className="flex flex-row justify-between pr-4 mt-4 ">
+            <div className="flex-col -space-y-2 text-black md:-space-y-5">
+              <p className="block text-4xl uppercase font-cormorant md:text-8xl md:hidden">
+                hof
+              </p>
+              <p className="text-4xl uppercase font-cormorant md:text-8xl">
+                on
+              </p>
+              <p className="text-4xl uppercase font-cormorant md:text-8xl ">
+                film
+              </p>
+            </div>
+            <nav className="flex flex-row items-end mb-1 ml-10 mr-4 space-x-3 text-black md:mb-3 md:mr-0">
+              <ul className="flex flex-row space-x-3">
+                <li>prints</li>
+                <li>about</li>
+                <li>35mm</li>
+              </ul>
+            </nav>
           </div>
         </div>
-
-        <div className="flex flex-row justify-between mt-4 pr-4 ">
-          <div className="flex-col -space-y-2 md:-space-y-5 text-black">
-            <p className=" font-cormorant text-4xl md:text-8xl uppercase block md:hidden ">
-              hof
-            </p>
-            <p className="font-cormorant text-4xl md:text-8xl uppercase">on</p>
-            <p className="font-cormorant text-4xl md:text-8xl uppercase ">
-              film
-            </p>
+        <div className="flex-col justify-center hidden m-4 md:w-2/6 5s:w-full md:flex">
+          <div className="w-full overflow-y-hidden h-5/6">
+            <Image
+              src="/traintracks.png"
+              width={538}
+              height={846}
+              layout="responsive"
+              quality={100}
+            />
           </div>
-          <nav className="flex flex-row items-end mb-1 md:mb-3 space-x-3 text-black ml-10  md:mr-0 mr-4">
-            <ul className="flex flex-row space-x-3">
-              <li>prints</li>
-              <li>about</li>
-              <li>35mm</li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-      <div className="md:w-2/6 5s:w-full justify-center  m-4 hidden md:flex ">
-        <div className="w-fill h-auto overflow-y-scroll">
-          <Image src="/traintracks.png" width={538} height={846} />
+          <div className="flex flex-row h-1/6">
+            <PicturePreview title="fomapan 100" subtitle="more from roll:" />
+            <PicturePreview image="/kickflipMork.jpg" />
+          </div>
         </div>
       </div>
     </div>
